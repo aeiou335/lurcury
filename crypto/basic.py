b59d7ee58d7b08af01b33f0647d88bcc42b1b941
zomlandlin <zomland.lin@imediplus.com>
2018-07-18 16:18:08 +0800

    #5 change sign return type

--- crypto/basic.py
+++ crypto/basic.py
@@ -58,21 +58,22 @@ class signature_c:
         priv = SigningKey.from_string(bytes().fromhex(priv),curve=SECP256k1)
         data = b(str(data))
         sig = priv.sign(data)
-        return binascii.hexlify(sig)
+        return binascii.hexlify(sig).decode()
     def verify(signData,rawData,pub):
         signData = binascii.unhexlify(signData)
-        pub = VerifyingKey.from_string(bytes().fromhex(pub))
+        pub = VerifyingKey.from_string(bytes().fromhex(pub),curve=SECP256k1)
         return pub.verify(signData, rawData)
     def exp():
         x = signature.sign("blahblah","24ac4b12bbb37e5b1e59830c7e376f1963b9cacb4233fa53")
         h = signature.verify(x,b("blahblah"),key.publicKey("24ac4b12bbb37e5b1e59830c7e376f1963b9cacb4233fa53"))
         return h
-print(Key_c.publicKey("97ddae0f3a25b92268175400149d65d6887b9cefaf28ea2c078e05cdc15a3c0a"))
-print(Key_c.address("0x7b83ad6afb1209f3c82ebeb08c0c5fa9bf6724548506f2fb4f991e2287a77090177316ca82b0bdf70cd9dee145c3002c0da1d92626449875972a27807b73b42e"))
-'''
-r = signature.sign("cxA665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3bx213ixA665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3nx02a82953d14e4495935fa884ea295b26c3ef877dfb9f8c366943cb1873711dfa42db6fbf1f3c4421c9c6dd95ad1dfafb6vx1pxixA665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3nx12a82953d14e4495935fa884ea295b26c3ef877dfb9f8c366943cb1873711dfa42db6fbf1f3c4421c9c6dd95ad1dfafb6vx1pxoxnx0cx67EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3vx1pxoxnx1cx67EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3vx1px","24ac4b12bbb37e5b1e59830c7e376f1963b9cacb4233fa53")
-print(r)
-'''
+#print(Key_c.publicKey("97ddae0f3a25b92268175400149d65d6887b9cefaf28ea2c078e05cdc15a3c0a"))
+#print(Key_c.address("7b83ad6afb1209f3c82ebeb08c0c5fa9bf6724548506f2fb4f991e2287a77090177316ca82b0bdf70cd9dee145c3002c0da1d92626449875972a27807b73b42e"))
+
+#r = signature_c.sign("123","97ddae0f3a25b92268175400149d65d6887b9cefaf28ea2c078e05cdc15a3c0a")
+#b = signature_c.verify(r,b"123",Key_c.publicKey("97ddae0f3a25b92268175400149d65d6887b9cefaf28ea2c078e05cdc15a3c0a"))
+#print(r)
+#print(b)
 
 #x = signature_c.sign("blahblah","f8b9fc996979291ac2968faeaedd88cd4c2fbc5611fda0605415e05eafc6658a")
 #print(x)
